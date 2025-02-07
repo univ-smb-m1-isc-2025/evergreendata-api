8a65b2a3747d7ee0d1ae4397f8c8fee4248a0c14
TomyElPapel <thomasburfinp@gmail.com>
2025-02-07 16:01:36 +0100

    init

---
+++ src/main/java/fr/fajitasmaster974/EvergreenData/EvergreenDataApplication.java
@@ -0,0 +1,13 @@
+package fr.fajitasmaster974.EvergreenData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvergreenDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvergreenDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/fajitasmaster974/EvergreenData/EvergreenDataApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fajitasmaster974.EvergreenData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvergreenDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
